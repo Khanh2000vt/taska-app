1ee3f502ad4c31f3c156c61baa0a8bd81836dd8d
Khánh Nguyễn Ngọc <khanhnguyenngoc@Khanhs-MacBook-Pro.local>
2023-06-25 19:13:01 +0700

    init folder src

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      // 'module-resolver',
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@firebase': './src/firebase',
+          '@hooks': './src/hooks',
+          '@interfaces': './src/interfaces',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@themes': './src/themes',
+          '@translations': './src/translations',
+          '@utils': './src/utils',
+          '@validates': './src/validates',
+          // "@i18n": "./src/i18n",
+          // "@routes": "./src/routes",
+          // "@services": "./src/services",
+          // '@env': './src/env.js',
+        },
+      },
+    ],
+  ],
 };
