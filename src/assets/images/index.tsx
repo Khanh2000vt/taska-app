4f472a4e61d24fd2fda87844c80243cb1e5cf41f
Khanh <khanh2000vt@gmail.com>
2023-06-25 22:32:48 +0700

    clean images

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,16 +1,8 @@
 const avatarDefault = require('./avatar-default.png');
-const backgroundCard = require('./background-card.png');
-const hotelPreview = require('./hotel-preview.png');
-const maps = require('./maps.png');
-const hotelSearchDefault = require('./hotel-search-default.png');
-const roomDefault = require('./room-default.png');
+const introCalendar = require('./intro-calendar.png');
 const Images = {
   avatarDefault,
-  backgroundCard,
-  hotelPreview,
-  maps,
-  hotelSearchDefault,
-  roomDefault,
+  introCalendar,
 };
 
 export {Images};
--- src/screens/auth/Intro/Intro.tsx
+++ src/screens/auth/Intro/Intro.tsx
@@ -1,14 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const Intro = () => {
+export const Intro = () => {
   return (
     <View>
       <Text>Intro</Text>
     </View>
   );
 };
-
-export default Intro;
 
 const styles = StyleSheet.create({});
