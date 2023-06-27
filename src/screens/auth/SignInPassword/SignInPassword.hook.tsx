f27b285b3cc13aa63e35fa3c6a63a9fba70c3fa5
Khanh <khanh2000vt@gmail.com>
2023-06-27 22:48:42 +0700

    copy login screen to sign in password screen

---
+++ src/screens/auth/SignInPassword/SignInPassword.hook.tsx
@@ -0,0 +1,90 @@
+import {Svgs} from '@assets';
+import {ASYNC_STORAGE, LANGUAGE} from '@constants';
+import {IFormikLogin, ILoginSocial, IOption} from '@interfaces';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {setUser} from '@redux';
+import i18next from 'i18next';
+import {useRef, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import {useDispatch} from 'react-redux';
+
+export const SignInPasswordHook = () => {
+  const initialValues: IFormikLogin = {
+    email: '',
+    password: '',
+  };
+
+  const dispatch = useDispatch();
+
+  const [rememberMe, setRememberMe] = useState<boolean>(false);
+
+  const refModal = useRef<any>(null);
+
+  const {t} = useTranslation();
+
+  // const listLoginSocial: ILoginSocial[] = [
+  //   {
+  //     id: 1,
+  //     icon: Svgs.Facebook,
+  //     onPress: () => {},
+  //   },
+  //   {
+  //     id: 2,
+  //     icon: Svgs.Google,
+  //     onPress: () => {},
+  //   },
+  //   {
+  //     id: 3,
+  //     icon: Svgs.Apple,
+  //     onPress: () => {},
+  //   },
+  // ];
+
+  const listLanguage: IOption<LANGUAGE>[] = [
+    {
+      id: 1,
+      label: 'English',
+      value: LANGUAGE.EN,
+      onPress: () => handleChangeLanguage(LANGUAGE.EN),
+    },
+    {
+      id: 2,
+      label: 'Việt Nam',
+      value: LANGUAGE.VN,
+      onPress: () => handleChangeLanguage(LANGUAGE.VN),
+    },
+  ];
+
+  const handleChangeLanguage = async (language: LANGUAGE) => {
+    try {
+      await AsyncStorage.setItem(ASYNC_STORAGE.LANGUAGE, language);
+      i18next.changeLanguage(language);
+      refModal.current?.close();
+    } catch (e) {}
+  };
+
+  const handleLogin = (values: IFormikLogin) => {
+    console.log('test: ');
+    dispatch(setUser({token: 'abc', user: {}}));
+  };
+
+  const handleRememberMe = (checked: boolean) => {
+    setRememberMe(checked);
+  };
+
+  const handlePressLanguage = () => {
+    refModal.current?.open();
+  };
+
+  return {
+    initialValues,
+    handleLogin,
+    handleRememberMe,
+    // listLoginSocial,
+    rememberMe,
+    listLanguage,
+    refModal,
+    handlePressLanguage,
+    t,
+  };
+};
---
+++ src/screens/auth/SignInPassword/SignInPassword.tsx
@@ -0,0 +1,135 @@
+import {Svgs} from '@assets';
+import {
+  AppButton,
+  AppCheckBox,
+  AppInputFormik,
+  AppModal,
+  AppText,
+  AppTouchable,
+  Block,
+  ItemOption,
+  LineView,
+  RowView,
+} from '@components';
+import {Colors, dimensions, Fonts, FontSize, scaler, Spacing} from '@themes';
+import {SignInSchema} from '@validates';
+import {Formik} from 'formik';
+import React from 'react';
+import {View} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {SignInPasswordHook} from './SignInPassword.hook';
+import {styles} from './SignInPassword.style';
+
+export const Login = () => {
+  const {
+    initialValues,
+    handleLogin,
+    handleRememberMe,
+    // listLoginSocial,
+    rememberMe,
+    listLanguage,
+    refModal,
+    handlePressLanguage,
+    t,
+  } = SignInPasswordHook();
+
+  return (
+    <Block block color={Colors.white}>
+      <SafeAreaView edges={['top', 'bottom']}>
+        <KeyboardAwareScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={styles.scroll}
+          bounces={false}>
+          <AppTouchable style={styles.btnLNG} onPress={handlePressLanguage}>
+            <AppText>LNG</AppText>
+          </AppTouchable>
+          <AppText size={FontSize.Font48} font={Fonts.fontWeight700}>
+            {t('auth.login.title')}
+          </AppText>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleLogin}
+            validationSchema={SignInSchema}>
+            {({handleSubmit, values}) => (
+              <>
+                <View style={styles.viewLogin}>
+                  <AppInputFormik
+                    labelFormik="email"
+                    placeholder={t('common.email') as string}
+                    // IconLeft={Svgs.EmailInput}
+                  />
+                  <AppInputFormik
+                    labelFormik="password"
+                    placeholder={t('common.password') as string}
+                    // IconLeft={Svgs.LockInput}
+                    password
+                  />
+                  <AppCheckBox
+                    checked={rememberMe}
+                    onChangeValue={handleRememberMe}
+                    label={t('auth.login.remember')}
+                    style={{justifyContent: 'center'}}
+                  />
+                  <AppButton
+                    label={t('auth.login.signIn')}
+                    onPress={handleSubmit}
+                    disabled={!(!!values.email && !!values.password)}
+                  />
+                  <AppTouchable>
+                    <AppText
+                      style={{textAlign: 'center'}}
+                      size={FontSize.Font16}
+                      color={Colors.primary}>
+                      {t('auth.login.forgot')}
+                    </AppText>
+                  </AppTouchable>
+                </View>
+                <View style={styles.viewLine}>
+                  <RowView gap={Spacing.width16} center>
+                    <LineView style={styles.line} />
+                    <AppText size={FontSize.Font18} color={Colors.gray.gray40}>
+                      {t('auth.login.continueOther')}
+                    </AppText>
+                    <LineView style={styles.line} />
+                  </RowView>
+                  <RowView gap={Spacing.width20} center>
+                    {/* {listLoginSocial.map(item => {
+                      return (
+                        <AppTouchable key={item.id} style={styles.buttonSocial}>
+                          <item.icon />
+                        </AppTouchable>
+                      );
+                    })} */}
+                  </RowView>
+                </View>
+                <RowView gap={Spacing.width8} center mt={scaler(100)}>
+                  <AppText>{t('auth.login.notAccount')}</AppText>
+                  <AppTouchable>
+                    <AppText color={Colors.primary}>
+                      {t('auth.login.signUp')}
+                    </AppText>
+                  </AppTouchable>
+                </RowView>
+              </>
+            )}
+          </Formik>
+        </KeyboardAwareScrollView>
+      </SafeAreaView>
+      <AppModal
+        ref={refModal}
+        modalSize={{height: scaler(200), width: dimensions.width}}
+        position="bottom">
+        {listLanguage.map(option => {
+          return (
+            <ItemOption
+              label={option.label}
+              onPress={option.onPress}
+              key={option.id}
+            />
+          );
+        })}
+      </AppModal>
+    </Block>
+  );
+};
